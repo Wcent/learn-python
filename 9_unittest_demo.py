2c7b83047d71abc073ecfef144c9b2f00d4c2624
Wcent <511737600@qq.com>
2017-06-11 01:36:35 +0800

    a demo shows how to use unittest module

---
+++ 9_unittest_demo.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'cent'
+
+'''
+a demo show how to use unittest module
+write unit test for dict class
+'''
+
+import unittest
+
+# all the test function must named with prefix 'test', otherwise it will not be run
+class TestDict(unittest.TestCase):
+    # override special function setUp(), run before every test function
+    def setUp(self):
+        print('setUp: Begin')
+
+    # override special function tearDown(), run after every test function
+    def tearDown(self):
+        print('tearDown: End')
+
+    def test_init(self):
+        d = dict(a=1, b='test')
+        self.assertIsInstance(d, dict)
+        self.assertTrue('a' in d)
+        self.assertTrue('b' in d)
+        self.assertEqual(d['a'], 1)
+        self.assertEqual(d['b'], 'test')
+
+    def test_key(self):
+        d = dict()
+        d['key'] = 'value'
+        self.assertTrue('key' in d)
+
+    def test_value(self):
+        d = dict()
+        d['key'] = 'value'
+        self.assertEqual(d['key'], 'value')
+
+    def test_key_error(self):
+        d = dict()
+        self.assertFalse('key' in d)
+
+        # assert to raise exception
+        with self.assertRaises(KeyError):
+            value = d['key']
+
+
+# testing to run all unit-test function
+if __name__ == '__main__':
+    unittest.main()
