394180e624e5e8b5bf9efeb551138074bca4365c
Wcent <511737600@qq.com>
2017-06-10 23:16:43 +0800

    demo about how to use exception to handle error at runtime

---
+++ exception_demo.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'cent'
+
+'''
+a demo about exception to handle error at runtime
+'''
+
+# testing try...exception...else...finally with division
+def divide(a, b):
+    try:
+        print('try...')
+        result = a / int(b)
+        print('result: ', result)
+        return result
+    except ValueError as e:
+        print('ValueError: ', e)
+    except ZeroDivisionError as e:
+        print('ZeroDivisionError: ', e)
+
+    # 异常为类，父类可以except捕获处理时，后面的子类不能再捕获，
+    except BaseException as e:
+        print('BaseException: ', e)
+
+    # 存在finally语句时，正常异常都会运行（注意，前面存在return时，依然运行finally，但End语句不会再跑）
+    finally:
+        print('finally...')
+    print('End')
+
+# testing
+if __name__ == '__main__':
+    divide(10, 'a')
+    divide(10, '0')
+    divide('10', 5)
+    divide(10, '3')
+    divide(10, 2)
