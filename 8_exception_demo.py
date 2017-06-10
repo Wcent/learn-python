aa35278e490bb0f6e4b9f0a21d814a7418faa02d
Wcent <511737600@qq.com>
2017-06-11 00:36:53 +0800

    test logging exception at runtime

--- 8_exception_demo.py
+++ 8_exception_demo.py
@@ -7,6 +7,11 @@ __author__ = 'cent'
 a demo about exception to handle error at runtime
 '''
 
+import logging
+
+# 设置日志文件log.log
+logging.basicConfig(filename='./log.log', level=logging.DEBUG)
+
 # testing try...exception...else...finally with division
 def divide(a, b):
     try:
@@ -22,6 +27,11 @@ def divide(a, b):
     # 异常为类，父类可以except捕获处理时，后面的子类不能再捕获，
     except BaseException as e:
         print('BaseException: ', e)
+        logging.exception(e)
+
+    # never run after BaseException
+    except Exception as e:
+        print('Exception: ', e)
 
     # 存在finally语句时，正常异常都会运行（注意，前面存在return时，依然运行finally，但End语句不会再跑）
     finally:
